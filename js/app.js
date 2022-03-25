700ac51a6d665c5917282e31d5cbcdf5d0d6f639
TazloCoding <tazlo2728@gmail.com>
2022-03-25 11:58:36 +0100

    Début du code affichage des images

--- js/app.js
+++ js/app.js
@@ -5,5 +5,52 @@ $(document).ready(function() {
     });
   });
  
-  
+  $('#case1').click(function() {
+    $('.img1').show(500);
+  })
+  $('#case2').click(function() {
+    $('.img2').show();
+  })
+  $('#case3').click(function() {
+    $('.img3').show();
+  })
+  $('#case4').click(function() {
+    $('.img4').show();
+  })
+  $('#case5').click(function() {
+    $('.img5').show();
+  })
+  $('#case6').click(function() {
+    $('.img6').show();
+  })
+  $('#case7').click(function() {
+    $('.img7').show();
+  })
+  $('#case8').click(function() {
+    $('.img8').show();
+  })
+  $('#case9').click(function() {
+    $('.img9').show();
+  })
+  $('#case10').click(function() {
+    $('.img10').show();
+  })
+  $('#case11').click(function() {
+    $('.img11').show();
+  })
+  $('#case12').click(function() {
+    $('.img12').show();
+  })
+  $('#case13').click(function() {
+    $('.img13').show();
+  })
+  $('#case14').click(function() {
+    $('.img14').show();
+  })
+  $('#case15').click(function() {
+    $('.img15').show();
+  })
+  $('#case16').click(function() {
+    $('.img16').show();
+  })
 })
